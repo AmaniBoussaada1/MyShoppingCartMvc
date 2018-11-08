992ee9c217a4233c72a7a3eba0fd2a2f4559ac3f
Amani Boussaada <amani.boussaada@ixin.net>
2018-11-08 17:54:26 +0100

    commit de la préparation des layers

---
+++ ShoppingCart.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShoppingCart.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShoppingCart.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a961ec1-762e-4792-a533-2b6b5fbceea6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ShoppingCart.Core/Facade/IProductsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart.Core.Domain;
+
+namespace ShoppingCart.Core.Facade
+{
+    public interface IProductsRepository
+    {
+        //CRUD
+        IEnumerable<Product> FindAll();
+
+    }
+}
