34d120f47afc38f38375d60d7eab8495141cdb83
Amani Boussaada <amani.boussaada@ixin.net>
2018-11-09 09:47:33 +0100

    Terminaison de l'affichage de la liste des produits

--- ShoppingCard.Web/App_Start/UnityConfig.cs
+++ ShoppingCard.Web/App_Start/UnityConfig.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
 using System.Web.Mvc;
+using Moq;
+using ShoppingCart.Core.Domain;
+using ShoppingCart.Core.Facade;
 using Unity;
 using Unity.Mvc5;
 
@@ -9,12 +13,16 @@ namespace ShoppingCard.Web
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
+            var mockProducts = new Mock<IProductsRepository>();
+            mockProducts.Setup(m => m.FindAll()).Returns(new List<Product>
+            {
+                new Product {ProductId = "P1000", Name = "Café", PhotoFile = "cafe.jpg", UnitPrice = 1.5M},
+                new Product {ProductId = "P2000", Name = "Thé", PhotoFile = "The.jpg", UnitPrice = 1.5M},
+                new Product {ProductId = "P3000", Name = "Chocoloat", PhotoFile = "Chocoloat.jpg", UnitPrice = 1.5M},
+                new Product {ProductId = "P4000", Name = "Gateau", PhotoFile = "Gateau.jpg", UnitPrice = 1.5M},
+            });
+            container.RegisterInstance(mockProducts.Object);
+          
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
